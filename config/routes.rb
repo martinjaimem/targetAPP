9511a5ceb65e667139677132b0d49f7770d6be25
Martin Moron <martinmoron@Martins-Mac-mini.local>
2019-05-30 14:42:23 -0300

    fix action cable configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: 'api/v1/users'
+  mount ActionCable.server => '/cable'
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       devise_scope :user do
