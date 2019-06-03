a845ebd23c8c20296c046a0793d7b6d180ad45a6
Martin Moron <martinmoron@Martins-Mac-mini.local>
2019-06-03 09:25:09 -0300

    Fix how errors were handled on target creation

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -5,9 +5,19 @@ module Api
 
       before_action :authenticate_user!, except: :status
 
+      rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
+
       # GET /status
       def status
         render json: { online: true }
+      end
+
+      def render_record_invalid(exception)
+        logger.info(exception)
+        render(
+          json: { errors: exception.record.errors.as_json },
+          status: :bad_request
+        )
       end
     end
   end
